bf96202b6cc92a1414dfe3895772c731c85fa2ef
Luiz Geraldo Pinheiro <luizpinheiro.rs@gmail.com>
2020-04-21 10:44:45 -0300

    RouteNavigation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
 import { Container, Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
+  // eslint-disable-next-line react/state-in-constructor
   state = {
     newRepo: '',
     repositories: [],
@@ -81,7 +83,9 @@ export default class Main extends Component {
           {repositories.map((repository) => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href=""> Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
